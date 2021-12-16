b00df83e79b6480de2660531f8d03af13c0b2c7b
rcerc <88944439+rcerc@users.noreply.github.com>
2021-12-16 12:57:05 -0500

    sway: add options to launch swayidle
    `swayidle.enable` starts swayidle with Sway and `swayidle.args` specifies arguments to pass to swayidle

--- modules/services/window-managers/i3-sway/sway.nix
+++ modules/services/window-managers/i3-sway/sway.nix
@@ -357,6 +357,25 @@ in {
       '';
     };
 
+    swayidle = {
+      enable = lib.mkOption rec {
+        type = lib.types.bool;
+        default = false;
+        example = !default;
+        description = ''
+          Whether to launch swayidle from Sway.
+        '';
+      };
+      args = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        example = [ "-d" "-w" ];
+        description = ''
+          Arguments to launch swayidle with.
+        '';
+      };
+    };
+
     wrapperFeatures = mkOption {
       type = wrapperOptions;
       default = { };
@@ -457,6 +476,13 @@ in {
           Requires = [ "graphical-session-pre.target" ];
         };
       };
+
+      wayland.windowManager.sway.config.startup =
+        lib.mkIf cfg.swayidle.enable [{
+          command = "${lib.getBin pkgs.swayidle}/bin/swayidle ${
+              lib.escapeShellArgs cfg.swayidle.args
+            }";
+        }];
     }
   ]);
 }
